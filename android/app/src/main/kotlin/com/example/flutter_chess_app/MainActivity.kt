e5183f31c685cf716b7faa85c699e018e47dec47
odman <okerdem.13@gmail.com>
2023-07-07 23:39:53 +0300

    Grid structure formed and colored.

---
+++ android/app/src/main/kotlin/com/example/flutter_chess_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_chess_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/game_board.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chess_app/components/square.dart';
+import 'helpers/helper_methods.dart';
+
+class GameBoard extends StatefulWidget {
+  const GameBoard({super.key});
+
+  @override
+  State<GameBoard> createState() => _GameBoardState();
+}
+
+class _GameBoardState extends State<GameBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.builder(
+          itemCount: 8 * 8,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 8),
+          itemBuilder: (context, index) {
+            return Square(isWhite: isWhite(index));
+          }),
+    );
+  }
+}
